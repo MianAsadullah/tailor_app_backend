f092db77a6543c7634d4df746582b5d72f6c7fcb
Asadullah <this.asadullah@gmail.com>
2026-06-21 02:16:40 +0500

    error fixed

--- api/index.ts
+++ api/index.ts
@@ -1,3 +1,4 @@
+import 'reflect-metadata';
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from '../server/src/app.module';
@@ -6,23 +7,35 @@ import { AllExceptionsFilter } from '../server/src/middlewares/error.middleware'
 let cachedApp: any;
 
 export default async function handler(req: any, res: any) {
-  if (!cachedApp) {
-    const app = await NestFactory.create(AppModule);
+  try {
+    if (!cachedApp) {
+      const app = await NestFactory.create(AppModule);
 
-    app.useGlobalFilters(new AllExceptionsFilter());
-    app.useGlobalPipes(
-      new ValidationPipe({
-        whitelist: true,
-        forbidNonWhitelisted: true,
-        transform: true,
-      }),
-    );
-    app.setGlobalPrefix('api');
+      app.useGlobalFilters(new AllExceptionsFilter());
+      app.useGlobalPipes(
+        new ValidationPipe({
+          whitelist: true,
+          forbidNonWhitelisted: true,
+          transform: true,
+        }),
+      );
+      app.setGlobalPrefix('api');
 
-    await app.init();
-    cachedApp = app.getHttpAdapter().getInstance();
+      await app.init();
+      cachedApp = app.getHttpAdapter().getInstance();
+    }
+
+    return cachedApp(req, res);
+  } catch (err) {
+    // Surface initialization/runtime errors to Vercel logs
+    console.error('Handler error:', err);
+    try {
+      res.statusCode = 500;
+      res.setHeader('Content-Type', 'application/json');
+      res.end(JSON.stringify({ statusCode: 500, message: 'Internal server error' }));
+    } catch (e) {
+      // ignore
+    }
   }
-
-  return cachedApp(req, res);
 }
 
